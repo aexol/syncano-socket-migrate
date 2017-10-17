7098ebbcaaddde567f14ebd075711e6bc41c5ec3
aexol <aexol@aexol.com>
2017-10-17 18:04:23 +0200

    c

--- src/helpers/transform.js
+++ src/helpers/transform.js
@@ -1,40 +1,69 @@
 import FormData from 'form-data'
 import axios from 'axios'
 const tempImage = 'data:image/gif;base64,R0lGODlhAQABAAAAACw='
-var request = require('request').defaults({ encoding: null });
-import JSON from 'json'
+var request = require('request').defaults({encoding: null})
 const transformFile = async f => {
   const {value, type} = f
-  const res = request.get(value,(err,res2,body)=>{
-  })
+  const res = request.get(value, (err, res2, body) => {})
   return [
     res,
     {
       filename: 'unicycle.png', // ... or:
-      filetype:'image/png'
+      filetype: 'image/png'
     }
   ]
 }
-export const transform = async o => {
+export const transform = async (o, data) => {
   let fd = new FormData()
-  let returnFormData = false
-  Object.keys(o).map(async k => {
-    let val = o[k]
-    if (
-      typeof val === 'object' &&
-      val.hasOwnProperty('type') &&
-      val.type === 'file'
-    ) {
-      returnFormData = true
-      val = await transformFile(val)
-    } else if (typeof val === 'object') {
-      val = [JSON.stringify(val)]
-    } else {
-      val = [val]
-    }
-    console.log(k, val)
-    fd.append(k, ...val)
-    return val
-  })
-  return returnFormData ? fd : o
+  const newo = await Promise.all(
+    Object.keys(o).map(async k => {
+      let val = o[k]
+      if (val === null) {
+        return
+      }
+      if (
+        typeof val === 'object' &&
+        val.hasOwnProperty('type') &&
+        val.type === 'file'
+      ) {
+        val = await transformFile(val)
+      } else if (
+        typeof val === 'object' &&
+        val.hasOwnProperty('type') &&
+        val.type === 'reference'
+      ) {
+        try {
+          val = [
+            (await data.migration_pair
+              .where('model', val.target)
+              .where('old', val.value)
+              .list())[0].new
+          ]
+        } catch (error) {
+          // TODO Handle error
+        }
+      } else if (
+        typeof val === 'object' &&
+        val.hasOwnProperty('type') &&
+        val.type === 'relation'
+      ) {
+        val = [
+          JSON.stringify(
+            (await data.migration_pair
+              .where('model', val.target)
+              .where('old', 'in', val.value)
+              .list()).map(o => o.new)
+          )
+        ]
+        // TODO Handle error
+      } else if (typeof val === 'object' || Array.isArray(val)) {
+        val = [JSON.stringify(val)]
+      } else {
+        val = [val]
+      }
+      fd.append(k, ...val)
+      return val
+    })
+  )
+  return fd
 }
--- src/migrate.js
+++ src/migrate.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 import Server from 'syncano-server'
 import {transform} from './helpers/transform'
+import NodeCache from 'node-cache'
+var linkCache = linkCache?linkCache:new NodeCache()
 export default async ctx => {
+  const MAX_OVERLOAD = 1000
   const {data, users, socket, response, event, logger, instance} = Server(ctx)
-  const {model, link, key, token, header, fields} = ctx.args
+  const {model, link, payload, key, token, header, fields} = ctx.args
   const {MIGRATE_KEY} = ctx.config
   const log = logger('Migrate')
   if (key === MIGRATE_KEY) {
@@ -14,18 +17,44 @@ export default async ctx => {
       }
     }
     try {
-      const oldObjects = (await axios.get(link, conf)).data
-      const objectsAlreadyInDatabase = (await data[model].list()).map(
-        m => m.migrate_id
-      )
-      console.log(objectsAlreadyInDatabase)
+      let status = await data.migration_status.where('model', model).list()
+      log.info(status)
+      if (status.length === 0) {
+        status = [
+          await data.migration_status.create({
+            model,
+            status: 'migrating',
+            mids: []
+          })
+        ]
+      }
+      let oldObjects
+      if (link) {
+        oldObjects = linkCache.get(link)
+        if (oldObjects === undefined) {
+          oldObjects = (await axios.get(link, conf)).data
+          linkCache.set(link, oldObjects, 10000)
+          log.info('Setting Cache')
+        } else {
+          log.info('Object gathered from Cache')
+        }
+      } else if (payload) {
+        oldObjects = typeof payload === 'string' ? JSON.parse(payload) : payload
+      }
+      const objectsAlreadyInDatabase = status.reduce((a, b) => {
+        return [...a, ...b.mids]
+      }, [])
       const createObject = oldObjects.filter(
         old => objectsAlreadyInDatabase.indexOf(old.id) === -1
       )
-      if (createObject.length === 0){
-        response.json({
-          status:"No more objects to migrate"
-        })
+      if (createObject.length === 0) {
+        return response.json(
+          {
+            status: 'end',
+            message: 'No more objects to migrate'
+          },
+          200
+        )
       }
       const objectReadyToMigrate = createObject[0]
       let newObject = {}
@@ -33,19 +62,51 @@ export default async ctx => {
         let fieldValue = objectReadyToMigrate[f]
         newObject[f] = fieldValue
       }
-      newObject['migrate_id'] = objectReadyToMigrate.id
-      const transformedObject = await transform(newObject)
+      const transformedObject = await transform(newObject, data)
       const obj = await data[model].create(transformedObject)
-      return response.json({
-        created:obj,
-        status:`${objectsAlreadyInDatabase.length+1}/${oldObjects.length}`
+      const migrateIdPair = await data.migration_pair.create({
+        model,
+        old: objectReadyToMigrate.id,
+        new: obj.id
       })
+      const migratingStatuses = status.filter(st => st.status === 'migrating')
+      if (migratingStatuses.length === 0) {
+        const newStatus = await data.migration_status.create({
+          model,
+          status: 'migrating',
+          mids: [objectReadyToMigrate.id],
+          progress: `${objectsAlreadyInDatabase.length + 1}/${oldObjects.length}`
+        })
+      } else {
+        const migratingStatus = migratingStatuses[0]
+        const newMids = [...migratingStatus.mids, objectReadyToMigrate.id]
+        const updatedStatus = await data.migration_status.update(
+          migratingStatus.id,
+          {
+            mids: newMids,
+            status: newMids.length >= MAX_OVERLOAD ? 'overload' : 'migrating',
+            progress: `${objectsAlreadyInDatabase.length + 1}/${oldObjects.length}`
+          }
+        )
+      }
+      return response.json(
+        {
+          created: obj,
+          status: 'migrating',
+          progress: `${objectsAlreadyInDatabase.length + 1}/${oldObjects.length}`
+        },
+        200
+      )
     } catch ({data}) {
       return response.json(data)
     }
   } else {
-    response.json({
-      status: 'Incorrect MIGRATE_KEY'
-    })
+    return response.json(
+      {
+        status: 'error',
+        message: 'Incorrect MIGRATE_KEY'
+      },
+      401
+    )
   }
 }
